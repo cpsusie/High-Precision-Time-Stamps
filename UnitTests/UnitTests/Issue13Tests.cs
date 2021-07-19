9cd5ab3a5091d01209dc0cc4eeaf3fb734dfeb4a
Christopher P. Susie <cpsusie@hotmail.com>
2021-07-19 17:16:16 -0400

    Elaboration of unit tests.

--- UnitTests/UnitTests/Issue13Tests.cs
+++ UnitTests/UnitTests/Issue13Tests.cs
@@ -16,11 +16,11 @@ namespace UnitTests
         public Issue13Tests([NotNull] ITestOutputHelper helper, 
             [NotNull] Issue13TextFixture fixture) : base(fixture, helper) {}
 
-        // [Fact]
-        // public void TryIt()
-        // {
-        //     GenerateAndSaveRoRefStuffToFile();
-        // }
+        //[Fact]
+        //public void TryIt()
+        //{
+        //    GenerateAndSaveRoRefStuffToFile();
+        //}
 
         [Fact]
         public void TestIdenticalConversionMethods()
@@ -33,12 +33,34 @@ namespace UnitTests
         }
 
         [Fact]
+        public void Test_Amzn2ToTemMilWinX64StrMismatch_1()
+        {
+            ref readonly Issue13StampTestPacket packet = ref Fixture.Amzn2ToTemMilWinX64StrMismatch_1;
+            string stringifiedHere = packet.PortableCastFromMonotonic.ToString();
+            string stringifiedThere = packet.PortableCastFromMonotonicStringified;
+            MonotonicStamp converted = (MonotonicStamp) packet.PortableCastFromMonotonic;
+            string convertedTxt = converted.ToString();
+            
+            PortableMonotonicStamp roundTripped = (PortableMonotonicStamp) converted;
+
+            Helper.WriteLine("Stringified here: {0}, stringified there: {1}", stringifiedHere, stringifiedThere);
+            Helper.WriteLine("Deserialized portable cast from monotonic.  UTC: {0}, Local: {1}", packet.PortableCastFromMonotonic.ToString(), packet.PortableCastFromMonotonic.ToLocalString());
+            Helper.WriteLine("Original monotonic string rep: {0}", packet.StringifiedMonotonicStamp);
+            Helper.WriteLine("Converted from portable local monotonic: {0}", convertedTxt);
+            Helper.WriteLine("Round tripped to portable - UTC: {0}, Local: {1}", roundTripped.ToString(), roundTripped.ToLocalString());
+            Helper.WriteLine("Stringified versions of serialized portable: {0}", packet.PortableCastFromMonotonicStringified);
+
+            Assert.Equal(roundTripped, packet.PortableCastFromMonotonic);
+            Assert.True(string.Equals(stringifiedHere, packet.PortableCastFromMonotonicStringified, StringComparison.Ordinal));
+            Assert.True(packet.CastMatchesString && packet.PortabledMatchesString);
+        }
+
+        [Fact]
         public void TryDeser()
         {
             var testSource = from item in Fixture.AllTestPackages
                 let title = item.Title
                 let xml = item.XmlContents
-                orderby title
                 select (Title: title, TestPackage: DeserXmlStr(xml));
             int itemCount = 0;
             foreach (var item in testSource)
@@ -48,9 +70,42 @@ namespace UnitTests
                     item.TestPackage.Count);
                 try
                 {
-                    Check(item.TestPackage, item.TestPackage.Count);
-                    Helper.WriteLine("\tTest #{0} PASSED.", itemCount );
+                    Check(item.TestPackage);
+                    Helper.WriteLine("\tTest #{0} PASSED.", itemCount);
                     Helper.WriteLine("END TEST NAMED {0}", item.Title);
+                }
+                catch (PacketBearingException ex)
+                {
+                    string reason = ex switch
+                    {
+                        InterpretationOverflowedException {}=> "Overflow",
+                        CastDoesNotEqualPortableException {}=> "CastPortableValueMismatch",
+                        StringsDoNotMatchException {} => "StringMismatch",
+                        _ => "Unknown"
+                    };
+                    Helper.WriteLine("Test failed due to {0}: {1}", reason, ex.Message);
+                    string badPacketXml = PacketToXmlString(in ex.BadPacket);
+                    FileInfo fi = null;
+                    try
+                    {
+                        fi = GetFailureTargetInfo(reason);
+                        {
+                            using var sw = fi.CreateText();
+                            sw.Write(badPacketXml);
+                        }
+                        fi.Refresh();
+                        if (!fi.Exists)
+                            throw new FileNotFoundException($"Unable to verify results saved to {fi.FullName}.");
+                        Helper.WriteLine($"Wrote failing test to {fi.FullName}.");
+                    }
+                    catch (Exception ex2)
+                    {
+                        Helper.WriteLine($"Unable to verify bad packet saved to {fi?.FullName ?? "UNKNOWN"} file.  (Error msg: {ex2.Message}.) Will log instead: {Environment.NewLine}");
+                        Helper.WriteLine(badPacketXml);
+                    }
+
+                    throw;
+
                 }
                 catch (Exception ex)
                 {
@@ -73,35 +128,44 @@ namespace UnitTests
         }
         
         
-        private void Check(ByRefRoList<Issue13StampTestPacket> packets, int expectCount)
+        private void Check(ByRefRoList<Issue13StampTestPacket> packets)
         {
-            int expectedStampMatch = expectCount;
-            int expectedStrMatch = expectCount;
-            
-            
-            Assert.Equal(expectCount, packets.Count);
-
-            int numCastEqualsToPortable = 0;
-            int numStringsMatch = 0;
-
             foreach (ref readonly var item in packets)
             {
-                if (item.CastEqualsToPortabled)
-                    ++numCastEqualsToPortable;
-                if (item.CastMatchesString && item.PortabledMatchesString)
-                    ++numStringsMatch;
+                try
+                {
+                    if (!item.CastMatchesString || !item.PortabledMatchesString)
+                        throw new StringsDoNotMatchException(in item);
+                    else if (!item.CastEqualsToPortabled)
+                        throw new CastDoesNotEqualPortableException(in item);
+                }
+                catch (PortableTimestampOverflowException ex)
+                {
+                    throw new InterpretationOverflowedException(in item, ex);
+                }
             }
-            Helper.WriteLine("\tFirst deserialized item: [{0}].", packets[0]);
-            Assert.Equal(expectedStampMatch, numCastEqualsToPortable);
-            Assert.Equal(expectedStrMatch, numStringsMatch);
-
             
+            Helper.WriteLine("\tFirst deserialized item: [{0}].", packets[0]);
         }
 
         private void ValidateSamePortableStamps(in Issue13StampTestPacket packet)
         {
             Assert.False(packet == default);
             Assert.True(packet.PortableCastFromMonotonic == packet.PortableToPortabledFromMonotonic);
+        }
+
+        private string PacketToXmlString(in Issue13StampTestPacket packet)
+        {
+            string xml;
+            {
+                using var memoryStream = new MemoryStream();
+                using var streamReader = new StreamReader(memoryStream);
+                DataContractSerializer serializer = new DataContractSerializer(typeof(Issue13StampTestPacket));
+                serializer.WriteObject(memoryStream, packet);
+                memoryStream.Position = 0;
+                xml = streamReader.ReadToEnd();
+            }
+            return xml;
         }
 
         private ByRefRoList<Issue13StampTestPacket> DeserXmlStr(string xml)
@@ -185,9 +249,135 @@ namespace UnitTests
             return testMe;
         }
 
-        private const string SysName = "AmznLinux2_1_000_000_000tps";
+        private FileInfo GetFailureTargetInfo(string failureType)
+        {
+            var stamp = MonotonicStampProvider.StampNow;
+            ref readonly var context = ref stamp.Context;
+            Helper.WriteLine("Ticks per second: {0}; Reference Time: {1:O}", context.TicksPerSecond, context.UtcDateTimeBeginReference);
+            string pathAttemptNoExt = $"{failureType}_{stamp.ToString().Replace(':', '-')}";
+            FileInfo testMe = new FileInfo(pathAttemptNoExt + Extension);
+            const int max = 100;
+            int tries = 0;
+            while (testMe.Exists && ++tries < max)
+            {
+                testMe = new FileInfo(pathAttemptNoExt + $"_{tries}" + Extension);
+            }
+
+            if (testMe.Exists)
+            {
+                throw new InvalidOperationException("too many files with that name already.");
+            }
+
+            return testMe;
+        }
+
+        //private const string SysName = "AmznLinux2_1_000_000_000tps";
         //private const string SysName = "Win10x64_2_441_442_tps";
-        //private const string SysName = "Win10x64_10_000_000_tps";
+        private const string SysName = "Win10x64_10_000_000_tps";
         private const string Extension = ".xml";
+    }
+
+    public sealed class CastDoesNotEqualPortableException : PacketBearingException
+    {
+        
+        public CastDoesNotEqualPortableException(in Issue13StampTestPacket badPacket) :
+            base(in badPacket, CreateMessage(in badPacket), null) {}
+
+        private static string CreateMessage(in Issue13StampTestPacket badPacket)
+            =>
+                $"The specified packet has a different cast portable (cast portable: {badPacket.PortableCastFromMonotonic}) value from its conversion method value: {badPacket.PortableToPortabledFromMonotonic}.";
+
+        
+    }
+
+    public sealed class StringsDoNotMatchException : PacketBearingException
+    {
+        public bool CastVersionMismatch => (_mismatchCode & MismatchedStrings.CastVersionDoesNotMatch) ==
+                                           MismatchedStrings.CastVersionDoesNotMatch;
+
+        public bool ToPortableDoesNotMatch => (_mismatchCode & MismatchedStrings.ToPortableVersionDoesNotMatch) ==
+                                               MismatchedStrings.ToPortableVersionDoesNotMatch;
+
+        public StringsDoNotMatchException(in Issue13StampTestPacket badPacket) : base(in badPacket, CreateMessage(in badPacket), null) =>
+            _mismatchCode = ExtractMismatch(in badPacket);
+        
+
+        private static string CreateMessage(in Issue13StampTestPacket badPacket)
+        {
+            MismatchedStrings mismatch = ExtractMismatch(in badPacket);
+
+            var context = MonotonicStampProvider.StampNow.Context;
+            string baseMsg =
+                $"The monotonic stamp acquired a source with {badPacket.AcqSysTicksPerSecond} ticks per " +
+                $"seconds (string rep of original monotonic: {badPacket.StringifiedMonotonicStamp}) then " +
+                $"converted to a portable stamp produces a different string representation in this environment " +
+                $"than it did in its original environment (this environment's ticks per second: {context.TicksPerSecond}).  ";
+
+            return baseMsg + mismatch switch
+            {
+
+                MismatchedStrings.BothMismatched => GetPortableMismatchString(in badPacket) + " " +
+                                                    GetCastMismatchedString(in badPacket),
+                MismatchedStrings.ToPortableVersionDoesNotMatch => GetPortableMismatchString(in badPacket),
+                MismatchedStrings.CastVersionDoesNotMatch => GetCastMismatchedString(in badPacket),
+                _ => "Unknown mismatch.",
+            };
+
+            static string GetPortableMismatchString(in Issue13StampTestPacket bp) =>
+                $"The \"to portable\" timestamp when stringified at source equaled: [{bp.PortableToStampedFromMonotonicStringified}] but when stringified here equaled: [{bp.PortableToPortabledFromMonotonic}].";
+            static string GetCastMismatchedString(in Issue13StampTestPacket bp2) => $"The \"CAST\" timestamp when stringified at source equaled: [{bp2.PortableCastFromMonotonicStringified}] but when stringified here equaled: [{bp2.PortableCastFromMonotonic}].";
+            
+        }
+
+        static MismatchedStrings ExtractMismatch(in Issue13StampTestPacket packet) => (
+                CastMismatch: !packet.CastMatchesString, ToPortableMismatch: !packet.PortabledMatchesString) switch
+            {
+                (true, true) => MismatchedStrings.BothMismatched,
+                (true, false) => MismatchedStrings.CastVersionDoesNotMatch,
+                (false, true) => MismatchedStrings.ToPortableVersionDoesNotMatch,
+                _ => MismatchedStrings.NoMismatch
+            };
+        
+
+        
+        private readonly MismatchedStrings _mismatchCode;
+    }
+
+    public sealed class InterpretationOverflowedException : PacketBearingException
+    {
+        public ref readonly MonotonicContext InterpretativeContext => ref _context;
+
+        public InterpretationOverflowedException(in Issue13StampTestPacket badPacket,
+            PortableTimestampOverflowException inner) : base(in badPacket, CreateMessage(in badPacket, inner), inner)
+        {
+            _context = MonotonicStampProvider.StampNow.Context;
+        }
+
+        private static string CreateMessage(in Issue13StampTestPacket badPacket, PortableTimestampOverflowException inner)
+        {
+            return $"The packet provided caused overflow when checking it: {badPacket}.  Exception: [{inner}]";
+        }
+
+       
+        private readonly MonotonicContext _context;
+    }
+
+    public abstract class PacketBearingException : ApplicationException
+    {
+        public ref readonly Issue13StampTestPacket BadPacket => ref _badPacket;
+
+        protected PacketBearingException(in Issue13StampTestPacket badPacket, string msg, Exception inner) : base(msg,
+            inner) => _badPacket = badPacket;
+
+        private readonly Issue13StampTestPacket _badPacket;
+    }
+
+    [Flags]
+    public enum MismatchedStrings : byte
+    {
+        NoMismatch = 0x00,
+        CastVersionDoesNotMatch = 0x01,
+        ToPortableVersionDoesNotMatch = 0x02,
+        BothMismatched = CastVersionDoesNotMatch | ToPortableVersionDoesNotMatch
     }
 }
--- UnitTests/UnitTests/Issue13TextFixture.cs
+++ UnitTests/UnitTests/Issue13TextFixture.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Runtime.Serialization;
+using Xunit;
 
 namespace UnitTests
 {
@@ -12,18 +14,33 @@ namespace UnitTests
         {
             get
             {
-                yield return AmznLinux2_x64_1000000000_Tps;
-                yield return Windows10x64_2441442_Tps;
+                yield return Win10_x64_10_000_000_Tps;
+                yield return AmznLinux2_x64_1_000_000_000_Tps;
+                yield return Windows10x64_2_441_442_Tps;
             }
         }
 
-        public (string XmlContents, string Title) Windows10x64_2441442_Tps { get; } =
+        public ref readonly Issue13StampTestPacket Amzn2ToTemMilWinX64StrMismatch_1 =>
+            ref TheStringificationMismatchAmzn2ToWinX64TenMil;
+
+        public (string XmlContents, string Title) Windows10x64_2_441_442_Tps { get; } =
             (ReadXmlFromPath(TheWin10x64_2_441_442_tps_2021_07_17T19_44_52_7102099_04_00_XmlPath),
                 "Windows 10 x64: 2,441,442 ticks per second.");
         
-        public (string XmlContents, string Title) AmznLinux2_x64_1000000000_Tps { get; } =
+        public (string XmlContents, string Title) AmznLinux2_x64_1_000_000_000_Tps { get; } =
             (ReadXmlFromPath(TheAmznLinux2_1_000_000_000tps_2021_07_17T21_26_46_2535718_04_00_XmlPath),
                 "Amazon Linux 2 x64:  1,000,000,000 ticks per second.");
+
+        public (string XmlContents, string Title) Win10_x64_10_000_000_Tps { get; } =
+            (ReadXmlFromPath(TheWin10x64_10_000_000tps__tps_2021_07_19T15_15_42_8424752_04_00_XmlPath),
+                "Windows 10 x64:  10,000,000 ticks per second.");
+
+        static Issue13TextFixture()
+        {
+            string stringError1Xml = ReadXmlFromPath(StringificationMismatchAmzn2ToWinX64TenMilXmlFile);
+            TheStringificationMismatchAmzn2ToWinX64TenMil = Deser(stringError1Xml);
+
+        }
 
         [JetBrains.Annotations.NotNull]
         private static string ReadXmlFromPath([JetBrains.Annotations.NotNull] string path)
@@ -32,12 +49,34 @@ namespace UnitTests
             using var sr = fi.OpenText();
             return sr.ReadToEnd();
         }
+
+        private static Issue13StampTestPacket Deser(string xml)
+        {
+            Issue13StampTestPacket packet;
+            {
+                using var stream = new MemoryStream();
+                byte[] data = System.Text.Encoding.UTF8.GetBytes(xml);
+                stream.Write(data, 0, data.Length);
+                stream.Position = 0;
+                DataContractSerializer deserializer = new DataContractSerializer(typeof(Issue13StampTestPacket));
+                packet = (Issue13StampTestPacket)deserializer.ReadObject(stream);
+            }
+            Assert.False(packet == default);
+            return packet;
+        }
         
         private const string TheWin10x64_2_441_442_tps_2021_07_17T19_44_52_7102099_04_00_XmlPath =
             @"../../../Resources/Win10x64_2_441_442_tps_2021-07-17T19-44-52.7102099-04-00.xml";
         private const string TheAmznLinux2_1_000_000_000tps_2021_07_17T21_26_46_2535718_04_00_XmlPath =
-            @"../../../Resources/AmznLinux2_1_000_000_000tps_2021-07-17T21-26-46.2535718-04-00.xml";        
-        
-        ///home/cpsusie/RiderProjects/High-Precision-Time-Stamps/UnitTests/UnitTests/bin/Debug/netcoreapp3.1/Resources
+            @"../../../Resources/AmznLinux2_1_000_000_000tps_2021-07-17T21-26-46.2535718-04-00.xml";
+        private const string TheWin10x64_10_000_000tps__tps_2021_07_19T15_15_42_8424752_04_00_XmlPath =
+            @"../../../Resources/Win10x64_10_000_000_tps_2021-07-19T15-15-42.8424752-04-00.xml";
+
+
+        private const string StringificationMismatchAmzn2ToWinX64TenMilXmlFile =
+            @"../../../Resources/StringMismatch_2021-07-19T16-47-26.5170050-04-00.xml";
+
+        private static readonly Issue13StampTestPacket TheStringificationMismatchAmzn2ToWinX64TenMil;
+        //home/cpsusie/RiderProjects/High-Precision-Time-Stamps/UnitTests/UnitTests/bin/Debug/netcoreapp3.1/Resources
     }
 }
